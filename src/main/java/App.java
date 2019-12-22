4e656afb690a284ac318e895c3066c2ccdbee57f
Elrokyd <elroky11111001001@gmail.com>
2019-12-22 22:26:43 +0300

    implemented name/value attributes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,13 +1,17 @@
 import model.Order;
 
+import java.io.File;
+
 public class App {
-    private static final String nameValue = "ВИД_ДОК";
+    private static final String nameValueTask1 = "ВИД_ДОК", nameValueTask2 = "ГРАЖДАНСТВО", stepValueTask2 = "1";
 
     public static void main(String[] args) {
+        File file = new File("resources/qwe.xml");
         XMLParser xmlParser = new XMLParser();
-        Order order = xmlParser.parse("D:\\IdeaProjects\\ParserSBERBANKUS\\src\\main\\resources\\qwe.xml");
+        Order order = xmlParser.parse(file.getAbsolutePath());
 
         OrderManager orderManager = new OrderManager();
-        orderManager.sortParLists(order, nameValue);
+        orderManager.sortParLists(order, nameValueTask1);
+        orderManager.sortParsAttribute(order, nameValueTask2, stepValueTask2);
     }
 }
--- src/main/java/OrderManager.java
+++ src/main/java/OrderManager.java
@@ -2,23 +2,55 @@ import model.Order;
 import model.Par;
 import model.ParList;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class OrderManager {
+    // Формирование коллекции, содержащая все виды документов
     void sortParLists(Order order, String nameValue) {
+        System.out.println("<<<<< TASK #1 >>>>>");
         order.getOrderServ().forEach(services -> {
             services.getServList().forEach(serv -> {
                 serv.getPars().forEach(pars -> {
-                    Optional<Par> optionalPar = pars.getPar().stream()
-                            .filter(par -> par.getName().equals(nameValue)).findFirst();
-
-                    optionalPar.ifPresent(par -> {
+                    List<Par> parList = pars.getPar().stream()
+                            .filter(par -> par.getName().equals(nameValue)).collect(Collectors.toList());
+                    parList.forEach(par -> {
                         List<ParList> result = par.getParListItems();
                         result.sort(Comparator.comparing(ParList::getValue));
-                        result.forEach(parList -> System.out.println(parList.getValue()));
+                        result.forEach(parListItem -> System.out.println(parListItem.getValue()));
+                        System.out.println("\n\n");
                     });
+                });
+            });
+        });
+        System.out.println("\n\n\n\n");
+    }
+    // Вывод имен и значений всех атрибутов для par с заданными атрибутами step и name
+    void sortParsAttribute(Order order, String nameValueTask2, String stepValue) {
+        System.out.println("<<<<< TASK #2 >>>>>");
+        order.getOrderServ().forEach(services -> {
+            services.getServList().forEach(serv -> {
+                serv.getPars().forEach(pars -> {
+                    pars.getPar().stream()
+                            .filter(par -> par.getName().equals(nameValueTask2) && (par.getStep().equals(stepValue)))
+                            .forEach(par -> {
+                                ArrayList<String> parNameVal = new ArrayList<>();
+                                Method[] methods = par.getClass().getMethods();
+                                for (Method method : methods) {
+                                    if (method.getName().startsWith("get") && !method.getName().equals("getParListItems") && !method.getName().equals("getClass")) {
+                                        try {
+                                            parNameVal.add(method.getName().replaceAll("(^get)", "") + "=" + (method.invoke(par) != null ? method.invoke(par) : ""));
+                                        } catch (IllegalAccessException | InvocationTargetException e) {
+                                        }
+                                    }
+                                }
+                                parNameVal.forEach(System.out::println);
+                                System.out.println("\n\n");
+                            });
                 });
             });
         });
